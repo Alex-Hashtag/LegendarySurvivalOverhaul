d35aed82eb434afab9942f18e19fa16c3577a9a9
Sfiomn <simon.fontaine91@gmail.com>
2025-06-18 12:56:11 +0200

    Add survival instinct compat Fix version prerequisite for ecliptic seasons

--- src/main/java/sfiomn/legendarysurvivaloverhaul/data/integration/providers/MedsAndHerbsBodyDamageProvider.java
+++ src/main/java/sfiomn/legendarysurvivaloverhaul/data/integration/providers/MedsAndHerbsBodyDamageProvider.java
@@ -1,6 +1,5 @@
 package sfiomn.legendarysurvivaloverhaul.data.integration.providers;
 
-import net.mcreator.medsandherbs.init.MedsAndHerbsModItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraftforge.common.data.ExistingFileHelper;
@@ -16,8 +15,8 @@ public class MedsAndHerbsBodyDamageProvider extends BodyDamageDataProvider {
 
     @Override
     public void generate(HolderLookup.Provider provider, ExistingFileHelper existingFileHelper) {
-        consumable(MedsAndHerbsModItems.MEDKIT_NOVICE.get()).healingCharges(0).healingValue(3).duration(400);
-        consumable(MedsAndHerbsModItems.MEDKIT_ADVANCED.get()).healingCharges(0).healingValue(5).duration(800);
-        consumable(MedsAndHerbsModItems.MEDKIT_EXPERT.get()).healingCharges(0).healingValue(8).duration(600);
+        consumable("medkit_novice").healingCharges(0).healingValue(3).duration(400);
+        consumable("medkit_advanced").healingCharges(0).healingValue(5).duration(800);
+        consumable("medkit_expert").healingCharges(0).healingValue(8).duration(600);
     }
 }
