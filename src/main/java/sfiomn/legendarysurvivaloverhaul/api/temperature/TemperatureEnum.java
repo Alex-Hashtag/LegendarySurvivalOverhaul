32e89f9b5b86badc42e9e6003eff6f9dd9de3fdf
Alex_Hashtag <aleksmihailov2015@gmail.com>
2026-01-09 21:13:02 +0100

    Fix for TempretureEnum

--- src/main/java/sfiomn/legendarysurvivaloverhaul/api/temperature/TemperatureEnum.java
+++ src/main/java/sfiomn/legendarysurvivaloverhaul/api/temperature/TemperatureEnum.java
@@ -1,14 +1,11 @@
 package sfiomn.legendarysurvivaloverhaul.api.temperature;
 
-// Stolen shamelessly from Charles445's SimpleDifficulty mod
-// https://github.com/Charles445/SimpleDifficulty/blob/v0.3.4/src/main/java/com/charles445/simpledifficulty/api/temperature/TemperatureEnum.java
-/// Every value uses the avarage of the 2 values for the bound!!!
 public enum TemperatureEnum
 {
     FROSTBITE(5),
-    COLD(13),
+    COLD(10),
     NORMAL(20),
-    HOT(27),
+    HOT(30),
     HEAT_STROKE(35);
 
     private final float value;
